1416c4b2379fc4880fac44ef0f01a980c818eb21
Avinash Patel <32591126+avijadu@users.noreply.github.com>
2021-04-08 09:54:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppToDoList from './AppToDoList';
+
+ReactDOM.render(
+    <AppToDoList/>
+    ,document.getElementById('root')
+);
+
